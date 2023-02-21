94a2693427b31d4546b42ce908247913c5502351
Jas <jaswithareddy@uchicago.edu>
2023-02-20 23:03:25 -0600

    Initial Commit

---
+++ Back to the Future/Back to the Future/Back_to_the_FutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Back_to_the_FutureApp.swift
+//  Back to the Future
+//
+//  Created by Jaswitha Reddy G on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Back_to_the_FutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
